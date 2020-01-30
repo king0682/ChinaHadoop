b47d94c5e24bdc299e3d7741f6abb646be9362aa
king0682 <wangye0682@gmail.com>
2020-01-30 10:39:39 +0800

    2020.01.30

---
+++ A1_python/test.py
@@ -0,0 +1,11 @@
+
+新款Powerbaets 4耳机来啦！外形像有线版Powerbeats Pro
+
+从更新的插图中我们可以看到Powerbaets 4与Powerbaets Pro非常相似，采用了更加弯曲的耳挂设计，与Powerbaets Pro有角度基本相同的环绕式耳钩，耳塞部分仍采用Powerbaets 3的设计，可以说是完美的将Powerbaets 3与Powerbaets Pro进行了融合。
+
+另外，我们可以清楚地看到在图片中耳钩的底部是有一条向下延伸的电线的，这说明Powerbaets 4并不是真无线耳机，应该还是回归系列的蓝牙运动耳机设计。
+
+
+值得一提的是，此次的Powerbaets 4将和Powerbaets Pro采用同款H1芯片，这是苹果真无线蓝牙耳机AirPods二代与AirPods Pro上的芯片。这会使得Powerbaets 4具有更好的连接性，还将能支持“Hey Siri”语音指令以及“通过Siri阅读消息”等功能。除了不是真无线耳机，Powerbaets 4的功能配置与Powerbaets Pro几乎和完全相同，价格可能会与Powerbaets 3类似，是一款性价比很高的选择。
+
+目前该款耳机的发布时间还不能确定，但从此前AirPods Pro曝光的时间来看，系统内出现产品图片后应该很快就会发布新品，大家敬请期待吧！（编辑：NJNR315）
